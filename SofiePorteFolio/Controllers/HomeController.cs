a1a1db58a6138127d51575845f94b4cb72287cc5
Sofie Lund Mogensen <1061659@ucn.dk>
2018-03-07 08:43:58 +0100

    Add project files.

---
+++ SofiePorteFolio/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SofiePorteFolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+         
+
+            return PartialView();
+        }
+
+        public ActionResult Skills()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Work()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Contact()
+        {
+          
+
+            return View();
+        }
+    }
+}
---
+++ SofiePorteFolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SofiePorteFolio.Startup))]
+namespace SofiePorteFolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
